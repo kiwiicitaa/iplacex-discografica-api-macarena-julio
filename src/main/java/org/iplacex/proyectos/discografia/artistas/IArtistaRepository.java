a9cb9c5d36a57e4143989eaa68e56e12efb3aa43
Macarena Julio <macarena.julio@alumno.iplacex.cl>
2026-07-19 14:26:39 -0400

    Update IArtistaRepository.java

--- src/main/java/org/iplacex/proyectos/discografia/artistas/IArtistaRepository.java
+++ src/main/java/org/iplacex/proyectos/discografia/artistas/IArtistaRepository.java
@@ -2,5 +2,7 @@ package org.iplacex.proyectos.discografia.artistas;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import org.iplacex.proyectos.discografia.artistas.Artista;
+
 public interface IArtistaRepository extends MongoRepository<Artista, String> {
 }
