575b129da8b29bfe493e154a60ddf4f6c8f92440
Macarena Julio <macarena.julio@alumno.iplacex.cl>
2026-07-18 20:06:00 -0400

    Update IDiscoRepository.java

--- src/main/java/org/iplacex/proyectos/discografia/discos/IDiscoRepository.java
+++ src/main/java/org/iplacex/proyectos/discografia/discos/IDiscoRepository.java
@@ -1,6 +1,5 @@
 package org.iplacex.proyectos.discografia.discos;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
-
+import org.iplacex.proyectos.discografia.discos.Disco;
 public interface IDiscoRepository extends MongoRepository<Disco, String> {
 }
